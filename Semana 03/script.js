b38b0ca6450c77733f97aa019262610c51462f8c
Elberth Lins Costa de Moraes <elberth.moraes@dataprev.gov.br>
2024-08-08 20:47:42 -0300

    Exibicao da quantidade de planetas

--- Semana 03/script.js
+++ Semana 03/script.js
@@ -6,4 +6,10 @@ planetas.push("Terra");
 planetas.push("Marte");
 planetas.push("Jupiter");
 
+alert("Tamanho do array: " + planetas.length);
+
+planetas[5] = "Saturno";
+
+alert("Tamanho do array: " + planetas.length);
+
 alert("Planetas: " + planetas + "!");
