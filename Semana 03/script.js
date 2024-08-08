b1879c7745409165a40409333a128b12f3622105
Elberth Lins Costa de Moraes <elberth.moraes@dataprev.gov.br>
2024-08-08 20:32:30 -0300

    Criacao do vetor de planetas

---
+++ Semana 03/script.js
@@ -0,0 +1,9 @@
+let planetas = [];
+
+planetas.push("Mercurio");
+planetas.push("Venus");
+planetas.push("Terra");
+planetas.push("Marte");
+planetas.push("Jupiter");
+
+alert("Planetas: " + planetas + "!");
